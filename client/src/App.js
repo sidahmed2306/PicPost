6d7eddb33f1befc5310a44a47a04b3fef5696778
sidahmed2306 <sidahmedbc1@gmail.com>
2023-02-10 15:53:23 +0100

    fix:Fixe fetch URL issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import SignUp from './pages/Sign-Up/SignUp';
-import LogIn from './pages/Log-In/LogIn';
-import SplashScreen from './pages/SplashScreen/SplashScreen';
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import SignUp from "./pages/Sign-Up/SignUp";
+import LogIn from "./pages/Log-In/LogIn";
+import SplashScreen from "./pages/SplashScreen/SplashScreen";
+import { useState } from "react";
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState("");
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<SplashScreen />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/log-in' setToken={setToken} element={<LogIn />} />
+          <Route path="/" element={<SplashScreen />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/log-in" element={<LogIn setToken={setToken} />} />
         </Routes>
       </BrowserRouter>
     </div>
