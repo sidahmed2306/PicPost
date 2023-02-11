73a17cd4b3dfbd9adb0ee5a9fc1cb066847bf2cb
MoSuperCode <mo.supercode@gmail.com>
2023-02-11 18:20:39 +0100

    Feat: implement Post layout and functionality, implement Searchpage functionality,implement Upload Page:upload from gallery

--- client/src/components/Search/Searchbar.js
+++ client/src/components/Search/Searchbar.js
@@ -1,1 +1,15 @@
+import { ReactComponent as Lupe } from '../../assets/img/lupe.svg';
+import { ReactComponent as ProfileIcon } from '../../assets/img/profileIcon.svg';
+import React from 'react';
 
+export default function Searchbar({ handleChange }) {
+    return (
+        <div>
+            <div>
+                <Lupe />
+                <input type="text" value={searchTerm} onChange={handleChange} />
+            </div>
+            <ProfileIcon />
+        </div>
+    );
+}
